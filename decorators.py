dcceb7788696ca86d83178e917d80924555b3d32
Ritesh.Jadhav <riteshjadhav1998.21@gmail.com>
2022-07-18 15:00:49 +0530

    decorator added

---
+++ decorators.py
@@ -0,0 +1,15 @@
+
+
+def fun1(fun2):
+    def fun3():
+        print("before working")
+        fun2()
+        print("after working")
+    return fun3
+
+@fun1
+def work():
+    print("I'm working")
+
+work()
+
